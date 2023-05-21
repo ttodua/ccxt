2d9365bcec9d71996de97eaeb348bf9d1de616c1
T.Todua <7117978+ttodua@users.noreply.github.com>
2023-05-21 12:46:47 +0400

    -

--- build.sh
+++ build.sh
@@ -69,11 +69,6 @@ ff () {
  
 # faster version of pre-transpile (without bundle and atomic linting)
 echo "zz1"
-cd $APPVEYOR_BUILD_FOLDER
-echo "zz1a"
-npm run export-exchanges2
-echo "zz1b"
-npm run export-exchanges
+cd $APPVEYOR_BUILD_FOLDER && npm run export-exchanges
 echo "zz2"
-node ./build/export-exchanges.js
 ff
