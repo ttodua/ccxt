1e4fbc9f1c46c4916013af52f5f74cc0a627b40d
T.Todua <7117978+ttodua@users.noreply.github.com>
2023-05-22 15:33:07 +0400

    -

--- appveyor-build.ps1
+++ appveyor-build.ps1
@@ -10,19 +10,3 @@ echo "2222"
 echo "%APPVEYOR_PULL_REQUEST_NUMBER%"
 echo "3333"
 echo "$APPVEYOR_PULL_REQUEST_NUMBER"
-
-### CHECK IF THIS IS A PR ###
-if ("%APPVEYOR_PULL_REQUEST_NUMBER%"="") {
-  echo "This is merger in master, will build everything"
-  build_and_test_all
-}
-
-
-##### DETECT CHANGES #####
-# temporarily remove the below scripts from diff
-# in appveyor, there is no origin/master locally, so we need to fetch it
-git remote set-branches origin 'master'
-git fetch --depth=1
-set diff=$(git diff origin/master --name-only)
-
-echo diff
