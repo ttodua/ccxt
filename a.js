985afac7bb07d4f008d3c335b484443086b49df4
T.Todua <7117978+ttodua@users.noreply.github.com>
2023-05-22 12:27:51 +0400

    -

--- a.js
+++ a.js
@@ -1,1 +1,4 @@
 console.log("a1 file was opened11111111111111111111111111");
+console.log(process.execPath);
+console.log("a1 file was opened222222222222222222222222222");
+console.log(process.argv);
