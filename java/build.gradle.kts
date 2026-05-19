d4850329a72101ec4bec030410ede98a0e2d792d
T.T <7117978+ttodua@users.noreply.github.com>
2026-05-19 23:39:38 +0400

    tmp

--- java/build.gradle.kts
+++ java/build.gradle.kts
@@ -3,4 +3,14 @@ subprojects {
         mavenCentral()
         // add more if you need them, e.g. mavenLocal(), google(), etc.
     }
+
+    plugins.withType<JavaPlugin> {
+        tasks.withType<JavaCompile>().configureEach {
+            options.compilerArgs.addAll(listOf(
+                "-Xlint:-varargs",
+                "-Xlint:-dep-ann"
+            ))
+            options.isFailOnError = false
+        }
+    }
 }
